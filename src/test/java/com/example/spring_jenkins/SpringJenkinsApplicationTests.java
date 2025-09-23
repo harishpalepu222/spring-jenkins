488d1fb0e6250a0d4e2ddcf40da4a705caaea2e5
unknown <harishpalepu222@gmail.com>
2025-09-23 19:10:51 +0530

    first commit

---
+++ src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.spring_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	public void contextLoads() {
+		logger.info(" Test case executing...");
+		int expected = 5;
+		int actual = 2 + 3;
+		assertEquals(expected, actual);
+	}
+
+}
